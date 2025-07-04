37ef38688108e9c8bc79b784d1b303e23f9c346f
r3ichal <maxim09p@gmail.com>
2025-07-04 09:20:00 +0300

    04.07.25: added linux compatibility, fixed some bugs

--- app/Common.cs
+++ app/Common.cs
@@ -12,80 +12,80 @@ using static LuaFlux.Functions;
 
 namespace LuaFlux
 {
-    internal class Common
+  internal class Common
+  {
+    internal static string TodosFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "LuaFlux", "todos.json");
+    internal static string SettingsFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "LuaFlux", "settings.json");
+
+    internal static string LuaFluxAssemblyVersion = Assembly.GetEntryAssembly().GetName().Version.ToString();
+    internal static string LuaFluxVersion = LuaFluxAssemblyVersion.Substring(0, LuaFluxAssemblyVersion.Length - 2);
+
+    internal List<LuaFluxTodoItem> Todos { get; set; } = new List<LuaFluxTodoItem>();
+
+    public class LuaFluxTodoItem
     {
-        internal static string TodosFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "LuaFlux", "todos.json");
-        internal static string SettingsFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "LuaFlux", "settings.json");
-
-        internal static string LuaFluxAssemblyVersion = Assembly.GetEntryAssembly().GetName().Version.ToString();
-        internal static string LuaFluxVersion = LuaFluxAssemblyVersion.Substring(0, LuaFluxAssemblyVersion.Length - 2);
-
-        internal List<LuaFluxTodoItem> Todos { get; set; } = new List<LuaFluxTodoItem>();
-
-        public class LuaFluxTodoItem
-        {
-            public int Id { get; set; } = 0;
-            public string Title { get; set; } = "";
-            public string Description { get; set; } = "";
+      public int Id { get; set; } = 0;
+      public string Title { get; set; } = "";
+      public string Description { get; set; } = "";
 
 
-            public Status TodoStatus { get; set; } = Status.Backlog;
-        }
-        public class TodoDisplayItem
-        {
-            public int Id { get; set; }
-            public string Title { get; set; }
-
-            public override string ToString() => $"{Id}: {Title}";
-        }
-
-        public class ENums
-        {
-            public enum Status
-            {
-                Backlog,
-                Todo,
-                Doing,
-                Done,
-                Frozen
-            }            
-        }
-
-        internal class Commands
-        {
-            public static Dictionary<string, string[]> _commandsDsc = new() // string[] = ["parameters", "description"]
-            {
-                ["help"] = ["", "shows every command and short description of them"],
-                ["clear"] = ["", "clears console."],
-                ["view"] = ["", "show list of all todos"],
-                ["create"] = ["", "create new todo"],
-                ["edit"] = ["", "edit existing todo"],
-                ["remove"] = ["", "removes existing todo"],
-                ["complete"] = ["", "Select a todo to mark it as done"],
-            };
-
-            public static Dictionary<string, string[]> _commandsAliases = new()
-            {
-                ["help"] = ["h", "g"],
-                ["clear"] = ["clr", "cls"],
-                ["test"] = ["t"],
-                ["view"] = ["v"],
-                ["create"] = ["c"],
-                ["remove"] = ["rm", "del", "delete"],
-                ["complete"] = ["done", "compl"],
-            };
-
-
-            public static Dictionary<string, Action<string, string>> _commands = new Dictionary<string, Action<string, string>>()
-            {
-                ["help"] = HelpFunction,
-                ["clear"] = ClearFunction,
-                ["view"] = ViewTodosFunction,
-                ["create"] = CreateTodoFunction,
-                ["edit"] = EditTodoFunction,
-                ["remove"] = RemoveTodoFunction,
-                ["complete"] = CompleteTodoFunction,
-            };
-        }
+      public Status TodoStatus { get; set; } = Status.Backlog;
     }
+    public class TodoDisplayItem
+    {
+      public int Id { get; set; }
+      public string Title { get; set; }
+
+      public override string ToString() => $"{Id}: {Title}";
+    }
+
+    public class ENums
+    {
+      public enum Status
+      {
+        Backlog,
+        Todo,
+        Doing,
+        Done,
+        Frozen
+      }
+    }
+
+    internal class Commands
+    {
+      public static Dictionary<string, string[]> _commandsDsc = new() // string[] = ["parameters", "description"]
+      {
+        ["help"] = ["", "shows every command and short description of them"],
+        ["clear"] = ["", "clears console."],
+        ["view"] = ["", "show list of all todos"],
+        ["create"] = ["", "create new todo"],
+        ["edit"] = ["", "edit existing todo"],
+        ["remove"] = ["", "removes existing todo"],
+        ["complete"] = ["", "Select a todo to mark it as done"],
+      };
+
+      public static Dictionary<string, string[]> _commandsAliases = new()
+      {
+        ["help"] = ["h", "g"],
+        ["clear"] = ["clr", "cls"],
+        ["test"] = ["t"],
+        ["view"] = ["v"],
+        ["create"] = ["c"],
+        ["remove"] = ["rm", "del", "delete"],
+        ["complete"] = ["done", "compl"],
+      };
+
+
+      public static Dictionary<string, Action<string, string>> _commands = new Dictionary<string, Action<string, string>>()
+      {
+        ["help"] = HelpFunction,
+        ["clear"] = ClearFunction,
+        ["view"] = ViewTodosFunction,
+        ["create"] = CreateTodoFunction,
+        ["edit"] = EditTodoFunction,
+        ["remove"] = RemoveTodoFunction,
+        ["complete"] = CompleteTodoFunction,
+      };
+    }
+  }
 }
--- app/Utilities.cs
+++ app/Utilities.cs
@@ -7,138 +7,154 @@ using Newtonsoft.Json;
 
 namespace LuaFlux
 {
-    internal class Utilities
+  internal class Utilities
+  {
+    public static readonly string TodosFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "LuaFlux", "todos.json");
+    public static int GenerateTodoId(List<Common.LuaFluxTodoItem> todos)
     {
-        public static readonly string TodosFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "LuaFlux", "todos.json");
-        public static int GenerateTodoId(List<Common.LuaFluxTodoItem> todos)
-        {
-            return todos.Count > 0 ? todos.Max(t => t.Id) + 1 : 1;
-        }
-
-        public static void LuaFluxScreen()
-        {
-            Console.ForegroundColor = ConsoleColor.DarkMagenta;
-            Console.WriteLine($"██╗     ██╗   ██╗ █████╗ ███████╗██╗     ██╗   ██╗██╗  ██╗ ");
-            Console.WriteLine($"██║     ██║   ██║██╔══██╗██╔════╝██║     ██║   ██║╚██╗██╔╝ ");
-            Console.WriteLine($"██║     ██║   ██║███████║█████╗  ██║     ██║   ██║ ╚███╔╝  ");
-            Console.WriteLine($"██║     ██║   ██║██╔══██║██╔══╝  ██║     ██║   ██║ ██╔██╗  v - {Common.LuaFluxVersion}");
-            Console.WriteLine($"███████╗╚██████╔╝██║  ██║██║     ███████╗╚██████╔╝██╔╝ ██╗ ");
-            Console.WriteLine($"╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚══════╝ ╚═════╝ ╚═╝  ╚═╝ ");
-
-            Console.WriteLine("Write h or help for list of commands, welcome to LuaFlux!");
-
-            Console.ResetColor();
-        }
-
-        public static void LuaFluxWaitAnimation()
-        {
-            Console.ForegroundColor = ConsoleColor.DarkMagenta;
-            Console.Write(">>");
-            Thread.Sleep(50);
-            Console.Write("\b");
-            Console.Write(">>");
-            Thread.Sleep(50);
-            Console.Write("\b");
-            Console.Write(">>");
-            Thread.Sleep(50);
-            Console.Write("\b");
-            Console.Write(": ");
-            Console.ResetColor();
-        }
-
-        public static string LuaFluxGetCommandFromAlias(string command)
-        {
-            foreach (KeyValuePair<string, string[]> entry in _commandsAliases)
-            {
-                if (entry.Value.Contains(command))
-                {
-                    return entry.Key;
-                }
-            }
-            return command;
-        }
-
-        public static void LuaFluxWrite(ConsoleColor color, string message)
-        {
-            Console.ForegroundColor = color;
-            Console.WriteLine(message);
-            Console.ResetColor();
-        }
-
-        public static void LuaFluxInitializeTodos()
-        {
-            if (!Directory.Exists(Path.GetDirectoryName(TodosFilePath)))
-            {
-                Directory.CreateDirectory(Path.GetDirectoryName(TodosFilePath));
-            }
-
-            if (!File.Exists(TodosFilePath))
-            {
-                var defaultTodos = CreateDefault<Todos>();
-                SaveToFile(TodosFilePath, defaultTodos);
-            }
-        }
-
-        public static T CreateDefault<T>() where T : new()
-        {
-            var instance = new T();
-            foreach (var property in typeof(T).GetProperties())
-            {
-                if (property.CanWrite)
-                {
-                    var defaultValue = property.PropertyType.IsValueType ? Activator.CreateInstance(property.PropertyType) : null;
-                    property.SetValue(instance, defaultValue);
-                }
-            }
-            return instance;
-        }
-
-        public static Todos LoadTodos()
-        {
-            return LoadFromFile<Todos>(TodosFilePath);
-        }
-
-        public static T LoadFromFile<T>(string filePath) where T : new()
-        {
-            if (!File.Exists(filePath))
-            {
-                LuaFluxInitializeTodos();
-            }
-
-            try
-            {
-                string json = File.ReadAllText(filePath);
-                return JsonConvert.DeserializeObject<T>(json) ?? CreateDefault<T>();
-            }
-            catch (JsonException ex)
-            {
-                LuaFluxWrite(ConsoleColor.Red, $"Failed to deserialize file. Error: {ex.Message}");
-                return CreateDefault<T>();
-            }
-        }
-
-        public static void SaveToFile<T>(string filePath, T obj)
-        {
-            string json = JsonConvert.SerializeObject(obj, Formatting.Indented);
-            File.WriteAllText(filePath, json);
-        }
+      return todos?.Count > 0 ? todos.Max(t => t.Id) + 1 : 1;
     }
 
-    internal class Todos
+    public static void LuaFluxScreen()
     {
-        public List<LuaFluxTodoItem> Items { get; set; } = new List<LuaFluxTodoItem>();
+      Console.ForegroundColor = ConsoleColor.DarkMagenta;
+      Console.WriteLine($"██╗     ██╗   ██╗ █████╗ ███████╗██╗     ██╗   ██╗██╗  ██╗ ");
+      Console.WriteLine($"██║     ██║   ██║██╔══██╗██╔════╝██║     ██║   ██║╚██╗██╔╝ ");
+      Console.WriteLine($"██║     ██║   ██║███████║█████╗  ██║     ██║   ██║ ╚███╔╝  ");
+      Console.WriteLine($"██║     ██║   ██║██╔══██║██╔══╝  ██║     ██║   ██║ ██╔██╗  v - {Common.LuaFluxVersion}");
+      Console.WriteLine($"███████╗╚██████╔╝██║  ██║██║     ███████╗╚██████╔╝██╔╝ ██╗ ");
+      Console.WriteLine($"╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚══════╝ ╚═════╝ ╚═╝  ╚═╝ ");
 
-        public void AddTodo(string title, string description)
+      Console.WriteLine("Write h or help for list of commands, welcome to LuaFlux!");
+
+      Console.ResetColor();
+    }
+
+    public static void LuaFluxWaitAnimation()
+    {
+      Console.ForegroundColor = ConsoleColor.DarkMagenta;
+      Console.Write(">>");
+      Thread.Sleep(50);
+      Console.Write("\b");
+      Console.Write(">>");
+      Thread.Sleep(50);
+      Console.Write("\b");
+      Console.Write(">>");
+      Thread.Sleep(50);
+      Console.Write("\b");
+      Console.Write(": ");
+      Console.ResetColor();
+    }
+
+    public static string LuaFluxGetCommandFromAlias(string command)
+    {
+      foreach (KeyValuePair<string, string[]> entry in _commandsAliases)
+      {
+        if (entry.Value.Contains(command))
         {
-            Items.Add(new LuaFluxTodoItem { Title = title, Description = description });
+          return entry.Key;
         }
+      }
+      return command;
+    }
 
-        public void RemoveTodoAt(int index)
+    public static void LuaFluxWrite(ConsoleColor color, string message)
+    {
+      Console.ForegroundColor = color;
+      Console.WriteLine(message);
+      Console.ResetColor();
+    }
+
+    public static void LuaFluxInitializeTodos()
+    {
+      if (!Directory.Exists(Path.GetDirectoryName(TodosFilePath)))
+      {
+        Directory.CreateDirectory(Path.GetDirectoryName(TodosFilePath));
+      }
+
+      if (!File.Exists(TodosFilePath))
+      {
+        var defaultTodos = CreateDefault<Todos>();
+        SaveToFile(TodosFilePath, defaultTodos);
+      }
+    }
+
+   public static T CreateDefault<T>() where T : new()
+{
+    var instance = new T();
+    foreach (var property in typeof(T).GetProperties())
+    {
+        if (property.CanWrite)
         {
-            if (index >= 0 && index < Items.Count)
+            object defaultValue;
+
+            if (property.PropertyType.IsGenericType &&
+                property.PropertyType.GetGenericTypeDefinition() == typeof(List<>))
             {
-                Items.RemoveAt(index);
+                defaultValue = Activator.CreateInstance(property.PropertyType);
             }
+            else if (property.PropertyType.IsValueType)
+            {
+                defaultValue = Activator.CreateInstance(property.PropertyType);
+            }
+            else
+            {
+                defaultValue = null;
+            }
+
+            property.SetValue(instance, defaultValue);
         }
     }
+    return instance;
+}
+
+
+    public static Todos LoadTodos()
+    {
+      return LoadFromFile<Todos>(TodosFilePath);
+    }
+
+    public static T LoadFromFile<T>(string filePath) where T : new()
+    {
+      if (!File.Exists(filePath))
+      {
+        LuaFluxInitializeTodos();
+      }
+
+      try
+      {
+        string json = File.ReadAllText(filePath);
+        return JsonConvert.DeserializeObject<T>(json) ?? CreateDefault<T>();
+      }
+      catch (JsonException ex)
+      {
+        LuaFluxWrite(ConsoleColor.Red, $"Failed to deserialize file. Error: {ex.Message}");
+        return CreateDefault<T>();
+      }
+    }
+
+    public static void SaveToFile<T>(string filePath, T obj)
+    {
+      string json = JsonConvert.SerializeObject(obj, Formatting.Indented);
+      File.WriteAllText(filePath, json);
+    }
+  }
+
+  internal class Todos
+  {
+    public List<LuaFluxTodoItem> Items { get; set; } = new List<LuaFluxTodoItem>();
+
+    public void AddTodo(string title, string description)
+    {
+      Items.Add(new LuaFluxTodoItem { Title = title, Description = description });
+    }
+
+    public void RemoveTodoAt(int index)
+    {
+      if (index >= 0 && index < Items.Count)
+      {
+        Items.RemoveAt(index);
+      }
+    }
+  }
 }
